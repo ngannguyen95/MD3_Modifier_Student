9305b885b5cd9b8b786be3a2036344fb2da4aa6f
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-03-13 11:58:26 +0700

    first commit

---
+++ src/rikeei/academy/Main.java
@@ -0,0 +1,9 @@
+package rikeei.academy;
+
+public class Main {
+    public static void main(String[] args) {
+        Student test=new Student();
+        test.setName("Marry");
+        System.out.println("Tên mặc định: "+test.getName()+"  tên lớp: "+ test.getClasses());
+    }
+}
